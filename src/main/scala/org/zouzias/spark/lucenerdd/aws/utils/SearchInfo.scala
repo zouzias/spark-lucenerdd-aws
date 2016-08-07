49e277f7ee56f4d3a9cba633edb96f6499a34c05
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-08-07 15:21:10 +0200

    remove search type from output file name

--- src/main/scala/org/zouzias/spark/lucenerdd/aws/utils/SearchInfo.scala
+++ src/main/scala/org/zouzias/spark/lucenerdd/aws/utils/SearchInfo.scala
@@ -6,6 +6,6 @@ import org.zouzias.spark.lucenerdd.aws.utils.SearchType.SearchType
 case class SearchInfo(searchType: SearchType, executorInstances: String, executorMemory: String, executorCores: String){
 
   override def toString(): String = {
-    s"${searchType.toString}-${executorInstances}-${executorMemory}-${executorCores}"
+    s"${executorInstances}-${executorMemory}-${executorCores}"
   }
 }
