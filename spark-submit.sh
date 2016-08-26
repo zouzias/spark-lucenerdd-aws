de37099e97ecbccce90680e0cb263b5f5b5631b5
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-08-26 18:26:47 +0200

    cleanup spark-submit

--- spark-submit.sh
+++ spark-submit.sh
@@ -11,8 +11,15 @@ SPARK_HOME=${HOME_DIR}/spark-1.6.2-bin-hadoop2.6
 MAIN_JAR=${CURRENT_DIR}/target/scala-2.10/spark-lucenerdd-aws-assembly-0.0.21.jar
 
 # Run spark shell locally
-${SPARK_HOME}/bin/spark-submit   --conf "spark.serializer=org.apache.spark.serializer.KryoSerializer" \
-				--conf "spark.kryo.registrator=org.zouzias.spark.lucenerdd.LuceneRDDKryoRegistrator" \
-				--master local[2] \
-				--class $1 \
-				"${MAIN_JAR}"
+${SPARK_HOME}/bin/spark-submit   \
+                 --conf spark.executor.memory=1g \
+                 --conf spark.driver.memory=1g \
+                 --conf spark.executor.cores=1 \
+                 --conf spark.executor.instances=2 \
+                 --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+				 --conf spark.kryo.registrator=org.zouzias.spark.lucenerdd.LuceneRDDKryoRegistrator \
+                 --conf spark.executor.extraJavaOptions="-Dlucenerdd.index.store.mode=memory" \
+                 --conf spark.driver.extraJavaOptions="-Dlucenerdd.index.store.mode=memory" \
+                 --master local[2] \
+				 --class $1 \
+				 "${MAIN_JAR}"
