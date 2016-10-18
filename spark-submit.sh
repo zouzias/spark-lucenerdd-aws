6d29e41e29f4249d3cf7ba7be092808533ae234e
Anastasios Zouzias <zouzias@users.noreply.github.com>
2016-10-18 17:02:53 +0200

    Update spark-submit.sh

--- spark-submit.sh
+++ spark-submit.sh
@@ -13,15 +13,12 @@ MAIN_JAR=${CURRENT_DIR}/target/scala-2.10/spark-lucenerdd-aws-assembly-0.0.24.ja
 # Run spark shell locally
 ${SPARK_HOME}/bin/spark-submit   \
                  --driver-memory 2g \
-		         --executor-memory 1g \
-                 --conf spark.executor.memory=1g \
-                 --conf spark.driver.memory=2g \
-                 --conf spark.executor.cores=1 \
+		 --executor-memory 4g \
                  --conf spark.executor.instances=2 \
                  --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
-				 --conf spark.kryo.registrator=org.zouzias.spark.lucenerdd.LuceneRDDKryoRegistrator \
+		 --conf spark.kryo.registrator=org.zouzias.spark.lucenerdd.LuceneRDDKryoRegistrator \
                  --conf spark.executor.extraJavaOptions="-Dlucenerdd.index.store.mode=memory" \
                  --conf spark.driver.extraJavaOptions="-Dlucenerdd.index.store.mode=memory" \
                  --master local[2] \
-				 --class $1 \
-				 "${MAIN_JAR}"
+		 --class $1 \
+		 "${MAIN_JAR}"
