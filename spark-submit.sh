e5816a48e4bafbb03aadda1a0494a4508ef83cb3
Anastasios Zouzias <zouzias@users.noreply.github.com>
2016-10-18 17:10:10 +0200

    Update spark-submit.sh

--- spark-submit.sh
+++ spark-submit.sh
@@ -2,23 +2,19 @@
 
 CURRENT_DIR=`pwd`
 
-# Assumes that spark is installed under home directory
-HOME_DIR=`echo ~`
-#export SPARK_LOCAL_IP=localhost
-SPARK_HOME=${HOME_DIR}/spark-1.6.2-bin-hadoop2.6
-
 # spark-lucenerdd assembly JAR
-MAIN_JAR=${CURRENT_DIR}/target/scala-2.10/spark-lucenerdd-aws-assembly-0.0.24.jar
+MAIN_JAR=${CURRENT_DIR}/target/scala-2.11/spark-lucenerdd-aws-assembly-0.2.1.jar
 
 # Run spark shell locally
-${SPARK_HOME}/bin/spark-submit   \
-                 --driver-memory 2g \
-		 --executor-memory 4g \
-                 --conf spark.executor.instances=2 \
-                 --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
-		 --conf spark.kryo.registrator=org.zouzias.spark.lucenerdd.LuceneRDDKryoRegistrator \
-                 --conf spark.executor.extraJavaOptions="-Dlucenerdd.index.store.mode=memory" \
-                 --conf spark.driver.extraJavaOptions="-Dlucenerdd.index.store.mode=memory" \
-                 --master local[2] \
-		 --class $1 \
-		 "${MAIN_JAR}"
+spark-submit   \
+	 --driver-memory 2g \
+	 --executor-memory 4g \
+	 --conf spark.executor.instances=4 \
+	 --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+	 --conf spark.kryo.registrator=org.zouzias.spark.lucenerdd.LuceneRDDKryoRegistrator \
+	 --conf spark.executor.extraJavaOptions="-Dlucenerdd.index.store.mode=disk" \
+	 --conf spark.driver.extraJavaOptions="-Dlucenerdd.index.store.mode=disk" \
+	 --master yarn \
+	 --deploy-mode client \
+	 --class $1 \
+	 "${MAIN_JAR}"
