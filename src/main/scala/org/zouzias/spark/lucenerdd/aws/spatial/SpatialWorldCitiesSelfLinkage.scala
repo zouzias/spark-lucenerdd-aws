741eab4cb11d046dde4044e4ad50c800a98472f1
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-10-25 08:01:58 +0200

    spatial: linkage: add example

---
+++ src/main/scala/org/zouzias/spark/lucenerdd/aws/spatial/SpatialWorldCitiesSelfLinkage.scala
@@ -0,0 +1,72 @@
+package org.zouzias.spark.lucenerdd.aws.spatial
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.zouzias.spark.lucenerdd.aws.linkage.ElapsedTime
+import org.zouzias.spark.lucenerdd.aws.utils.{LinkedSpatialRecord, Utils, WikipediaUtils}
+import org.zouzias.spark.lucenerdd.logging.Logging
+import org.zouzias.spark.lucenerdd.spatial.shape.ShapeLuceneRDD
+import org.zouzias.spark.lucenerdd.spatial.shape._
+import org.zouzias.spark.lucenerdd._
+
+/**
+ * Spatial world cities self-linkage
+ */
+object SpatialWorldCitiesSelfLinkage extends Logging {
+
+    def main(args: Array[String]) {
+
+      // initialise spark context
+      val conf = new SparkConf().setAppName(SpatialWorldCitiesSelfLinkage.getClass.getName)
+
+      implicit val spark = SparkSession.builder().config(conf).getOrCreate()
+      import spark.implicits._
+
+      val today = WikipediaUtils.dayString()
+      val executorMemory = conf.get("spark.executor.memory")
+      val executorCores = conf.get("spark.executor.cores")
+      val executorInstances = conf.get("spark.executor.instances")
+      val fieldName = "City"
+
+
+      log.info(s"Executor instances: ${executorInstances}")
+      log.info(s"Executor cores: ${executorCores}")
+      log.info(s"Executor memory: ${executorMemory}")
+
+      val start = System.currentTimeMillis()
+
+      val citiesDF = spark.read.parquet("s3://recordlinkage/world-cities-maxmind.parquet").repartition(60)
+      citiesDF.cache
+      val total = citiesDF.count
+      logInfo(s"Cities: ${total}")
+
+      val cities = citiesDF.select("Latitude", "Longitude", "City", "Country")
+        .map(row => ((row.getString(1).toDouble, row.getString(0).toDouble), (row.getString(2), row.getString(3))))
+
+
+      val shapes = ShapeLuceneRDD(cities)
+
+      shapes.cache
+      shapes.count
+      logInfo("Max mind cities loaded successfully")
+
+      // Link and fetch top-3
+      val linkage = shapes.linkByRadius(cities.rdd, {x:((Double, Double), (String, String)) => x._1}, 3)
+
+
+      import spark.implicits._
+      val linkedDF = spark.createDataFrame(linkage.map{ case (left, right) => LinkedSpatialRecord(left._2, right.headOption.flatMap(_.doc.textField(fieldName)))})
+
+      linkedDF.write.mode(SaveMode.Overwrite)
+        .parquet(s"s3://spark-lucenerdd/timings/v${Utils.Version}/max-mind-cities-linkage-result-${today}-${executorMemory}-${executorInstances}-${executorCores}.parquet")
+
+      val end = System.currentTimeMillis()
+
+      spark.createDataFrame(Seq(ElapsedTime(start, end, end - start))).write.mode(SaveMode.Overwrite)
+        .parquet(s"s3://spark-lucenerdd/timings/v${Utils.Version}/max-mind-cities-linkage-timing-${today}-${executorMemory}-${executorInstances}-${executorCores}.parquet")
+
+      // terminate spark context
+      spark.stop()
+    }
+
+}
--- src/main/scala/org/zouzias/spark/lucenerdd/aws/utils/LinkedRecord.scala
+++ src/main/scala/org/zouzias/spark/lucenerdd/aws/utils/LinkedRecord.scala
@@ -1,3 +1,5 @@
 package org.zouzias.spark.lucenerdd.aws.utils
 
 case class LinkedRecord(left: String, right: Option[Array[String]])
+
+case class LinkedSpatialRecord(left: (String, String), right: Option[String])
