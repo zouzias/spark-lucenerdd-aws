9b0a4ac437a9a3dc424e7f5fd08bddfa4879084b
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-11-26 20:08:46 +0100

    index paragraphs wikipedia

---
+++ spark-index-pages-wikipedia.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CURRENT_DIR=`pwd`
+
+# Read the version from version.sbt
+SPARK_LUCENERDD_VERSION=`cat version.sbt | awk '{print $5}' | xargs`
+
+echo "==============================================="
+echo "Loading LuceneRDD with version ${SPARK_LUCENERDD_VERSION}"
+echo "==============================================="
+# spark-lucenerdd assembly JAR
+MAIN_JAR=${CURRENT_DIR}/target/scala-2.11/spark-lucenerdd-aws-assembly-${SPARK_LUCENERDD_VERSION}.jar
+
+# Run spark shell locally
+# Run spark shell locally
+spark-submit   \
+	 --driver-memory 4g \
+	 --executor-memory 6g \
+	 --conf spark.executor.instances=10 \
+	 --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+	 --conf spark.kryo.registrator=org.zouzias.spark.lucenerdd.spatial.shape.ShapeLuceneRDDKryoRegistrator \
+	 --conf spark.executor.extraJavaOptions="-Dlucenerdd.index.store.mode=disk" \
+	 --conf spark.driver.extraJavaOptions="-Dlucenerdd.index.store.mode=disk" \
+	 --master yarn \
+	 --deploy-mode client \
+	 --class org.zouzias.spark.lucenerdd.aws.indexing.WikipediaPagesIndexingExample \
+	 "${MAIN_JAR}"
---
+++ src/main/scala/org/zouzias/spark/lucenerdd/aws/indexing/WikipediaPagesIndexingExample.scala
@@ -0,0 +1,53 @@
+package org.zouzias.spark.lucenerdd.aws.indexing
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.zouzias.spark.lucenerdd.aws.utils._
+import org.zouzias.spark.lucenerdd.logging.Logging
+import org.zouzias.spark.lucenerdd.{LuceneRDD, _}
+
+
+/**
+ * Wikipedia indexing performance test
+ */
+object WikipediaPagesIndexingExample extends Logging {
+
+  def main(args: Array[String]) {
+
+    // initialise spark context
+    val conf = new SparkConf().setAppName("WikipediaPagesIndexingExample")
+    implicit val spark = SparkSession.builder().config(conf).getOrCreate()
+
+    val today = Utils.dayString()
+    val executorMemory = conf.get("spark.executor.memory")
+    val executorCores = conf.get("spark.executor.cores")
+    val executorInstances = conf.get("spark.executor.instances")
+
+    log.info(s"Executor instances: ${executorInstances}")
+    log.info(s"Executor cores: ${executorCores}")
+    log.info(s"Executor memory: ${executorMemory}")
+
+    logInfo("Loading Wikipedia titles")
+    val sparkInfo = SparkInfo(executorInstances, executorMemory, executorCores)
+
+    import spark.implicits._
+    val wiki = spark.read.parquet("s3://spark-lucenerdd/wikipedia/enwiki-pages-20161124.parquet")
+        .select("paragraphs").map(row => row.getString(0)).map(_.replaceAll("_", " ")).map(_.replaceAll("[^a-zA-Z0-9\\s]", ""))
+      .rdd
+
+    logInfo("Wikipedia titles loaded successfully")
+
+    val start = System.currentTimeMillis()
+    val luceneRDD = LuceneRDD(wiki)
+    luceneRDD.cache()
+    luceneRDD.count() // To force caching
+    val end = System.currentTimeMillis()
+
+    import spark.implicits._
+    val timingsDF = spark.sparkContext.parallelize(Array(IndexingTiming(end - start, today, Utils.Version))).toDF()
+    timingsDF.write.mode(SaveMode.Append).parquet(s"s3://spark-lucenerdd/timings/wikipedia-indexing-pages-${sparkInfo.toString()}.parquet")
+
+    // terminate spark context
+    spark.stop()
+  }
+}
