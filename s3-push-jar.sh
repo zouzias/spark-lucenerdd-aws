a7623de2f5cda88f62f755383e66811c2d826bbb
Anastasios Zouzias <zouzias@gmail.com>
2018-07-24 10:20:56 +0200

    scripts: push to aws/ dir on S3

--- s3-push-jar.sh
+++ s3-push-jar.sh
@@ -8,4 +8,4 @@ sbt assembly
 # Push to S3 (assuming aws-cli is properly setup)
 SPARK_LUCENERDD_VERSION=`cat version.sbt | awk '{print $5}' | xargs`
 echo "Pushing Spark LuceneRDD version ${SPARK_LUCENERDD_VERSION} to S3"
-aws s3 cp target/scala-2.11/spark-lucenerdd-aws-assembly-${SPARK_LUCENERDD_VERSION}.jar s3://spark-lucenerdd/2.11/
+aws s3 cp target/scala-2.11/spark-lucenerdd-aws-assembly-${SPARK_LUCENERDD_VERSION}.jar s3://spark-lucenerdd/aws/
